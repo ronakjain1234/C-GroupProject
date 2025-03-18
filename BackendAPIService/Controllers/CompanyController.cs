0105a4594e875231632d86e47eb7282550367ad2
ronakjain1234 <ronakjain0919@utexas.edu>
2025-03-18 02:34:37 -0500

    Added get companies request

--- BackendAPIService/Controllers/CompanyController.cs
+++ BackendAPIService/Controllers/CompanyController.cs
@@ -20,8 +20,16 @@ public class CompanyController : ControllerBase
     [Route("get")]
     public ActionResult<List<Web.Company>> Get(int userID, int limit = 50, int offset = 0, string? searchString = null)
     {
-        return Ok(_dbContext.Companies);
-        throw new NotImplementedException();
+        try
+        {
+            var allCompanies = _dbContext.Companies;
+            return Ok(allCompanies);
+        } catch (Exception ex) 
+        {   
+            Console.WriteLine("An error occured: {0}", ex.Message);
+            return StatusCode(500, new SimpleErrorResponse { Message = "An error occurred while fetching companies."});
+        }
+        
     }
 
     [HttpGet]
