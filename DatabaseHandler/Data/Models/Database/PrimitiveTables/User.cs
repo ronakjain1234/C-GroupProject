7e530a24eb4861a5ec654482dfca61fd928c220a
ronakjain1234 <ronakjain0919@utexas.edu>
2025-03-18 08:45:03 -0500

    Created getting user endpoint

--- DatabaseHandler/Data/Models/Database/PrimitiveTables/User.cs
+++ DatabaseHandler/Data/Models/Database/PrimitiveTables/User.cs
@@ -7,6 +7,8 @@ public class User
     [Key]
     public int UserID { get; set; }
     public string Name { get; set; }
+    public string Email {get; set; }
+    public string Roles{get; set;}
     
     [ConcurrencyCheck]
     public DateTime LastChange {get; set;}
--- DatabaseHandler/Migrations/20250318131924_UpdatedUserSchema.Designer.cs
+++ DatabaseHandler/Migrations/20250318131924_UpdatedUserSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DatabaseHandler;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatabaseHandler.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250318131924_UpdatedUserSchema")]
+    partial class UpdatedUserSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
@@ -268,7 +271,7 @@ namespace DatabaseHandler.Migrations
                         .IsRequired()
                         .HasColumnType("TEXT");
 
-                    b.Property<string>("Roles")
+                    b.Property<string>("RoleName")
                         .IsRequired()
                         .HasColumnType("TEXT");
 
