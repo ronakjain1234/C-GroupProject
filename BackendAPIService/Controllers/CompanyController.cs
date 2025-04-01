87027bde445ea6ecb08149c5801ceaadb896e86a
ronakjain1234 <ronakjain0919@utexas.edu>
2025-04-01 10:02:23 -0500

    EditName Endpoint done

--- BackendAPIService/Controllers/CompanyController.cs
+++ BackendAPIService/Controllers/CompanyController.cs
@@ -4,6 +4,8 @@ using Database = DatabaseHandler.Data.Models.Database;
 using Web = MyMudBlazorApp.Objects;
 using Microsoft.AspNetCore.Mvc;
 using DatabaseHandler.Data.Models.Database.MixedTables;
+using MyMudBlazorApp.Pages;
+using DatabaseHandler.Data.Models.Database;
 namespace BackendAPIService.Controllers;
 
 [ApiController]
@@ -129,6 +131,44 @@ public class CompanyController : ControllerBase
             transaction.Rollback();
             Console.WriteLine("An error occured: {0}", ex.Message);
             return StatusCode(500, new SimpleErrorResponse {Success = false, Message = "An error occurred while creating the user"});
+        }
+    }
+
+    [HttpPut]
+    [Route("changeCompanyName")]
+
+    public ActionResult<SimpleErrorResponse> ChangeCompanyName (int userId, int companyId, string companyName)
+    {
+        try
+        { 
+            var hasAccess = _dbContext.UserCompanies.Any(uc => uc.CompanyID == companyId && uc.UserID == userId);
+            if (!hasAccess)
+            {
+                return StatusCode(500, new SimpleErrorResponse {Success = false, Message = "User does not have access"});
+            }
+
+            if (string.IsNullOrEmpty(companyName))
+            {
+                return StatusCode(400, new SimpleErrorResponse { Success = false, Message = "Company name cannot be empty." });
+            }
+
+            var existingCompany = _dbContext.Companies.FirstOrDefault(c => c.CompanyID == companyId);
+            if (existingCompany == null)
+            {
+                return StatusCode(404, new SimpleErrorResponse { Success = false, Message = "Company not found." });
+            }
+
+            existingCompany.CompanyName = companyName;
+
+            _dbContext.SaveChanges();
+
+            return StatusCode(200, new SimpleErrorResponse { Success = true, Message = "Successfully updated the company." });
+
+
+        } catch(Exception ex)  
+        {
+            Console.WriteLine("An error occurred: {0}", ex.Message);
+            return StatusCode(500, new SimpleErrorResponse { Success = false, Message = "An error occurred while editing the company." });
         }
     }
 }
