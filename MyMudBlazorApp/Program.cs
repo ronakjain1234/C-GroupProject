274b6c502948eb620b9bb41183e48fd1d4ef6123
catalina-mihai <79297440+catalina-mihai@users.noreply.github.com>
2025-04-25 08:56:47 +0200

    Update Program.cs

--- MyMudBlazorApp/Program.cs
+++ MyMudBlazorApp/Program.cs
@@ -8,9 +8,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 builder.Services.AddScoped<ILogService, LogService>();
 
 await builder.Build().RunAsync();
+
